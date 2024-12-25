cba750d53e7248f779484dbb27705a78938d3f3a
sohailaZaki <sohailazakifcis@gmail.com>
2024-12-25 14:59:04 +0200

    CRUD on worker

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,10 @@ module com.example.hotelmanagementsystem {
     opens com.example.hotelmanagementsystem.manager to javafx.fxml;
     opens com.example.hotelmanagementsystem.login to javafx.fxml;
     opens com.example.hotelmanagementsystem to javafx.fxml;
+
     exports com.example.hotelmanagementsystem.receptionist; // Ensure that the package is exported
     exports com.example.hotelmanagementsystem;
+    exports com.example.hotelmanagementsystem.manager;
     exports com.example.hotelmanagementsystem.receptionist.Decorator;
     opens com.example.hotelmanagementsystem.receptionist.Decorator to javafx.fxml;
     exports com.example.hotelmanagementsystem.receptionist.controller;
@@ -19,4 +21,10 @@ module com.example.hotelmanagementsystem {
     opens com.example.hotelmanagementsystem.receptionist.Models to javafx.fxml;
     exports com.example.hotelmanagementsystem.receptionist.Strategy;
     opens com.example.hotelmanagementsystem.receptionist.Strategy to javafx.fxml;
+    exports com.example.hotelmanagementsystem.manager.controllers;
+    opens com.example.hotelmanagementsystem.manager.controllers to javafx.fxml;
+    exports com.example.hotelmanagementsystem.manager.Classes.Manager;
+    opens com.example.hotelmanagementsystem.manager.Classes.Manager to javafx.fxml;
+    exports com.example.hotelmanagementsystem.manager.Classes.Workers;
+    opens com.example.hotelmanagementsystem.manager.Classes.Workers to javafx.fxml;
 }
